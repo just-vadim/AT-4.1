75fed92ad4bc94c71d2a3287d0473c677c4ea500
Vadim <vadim.kurn@gmail.com>
2020-12-03 11:57:22 +0300

    tests added

--- src/test/java/ru/notology/appcarddelivery/AppCardDeliveryTest.java
+++ src/test/java/ru/notology/appcarddelivery/AppCardDeliveryTest.java
@@ -17,7 +17,7 @@ public class AppCardDeliveryTest {
 
     //Метод генерирует случайную дату в диапазоне от +3 до +30 дней от сегодняшней даты
     String generateDate() {
-        final Random random = new Random();
+        Random random = new Random();
         int randomInt = random.nextInt(31) + 3;
         LocalDate date = LocalDate.now().plusDays(randomInt);
         String formattedDate = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
@@ -38,5 +38,175 @@ public class AppCardDeliveryTest {
         $(".button__content").click();
         String text = $(withText("Встреча успешно")).waitUntil(Condition.visible, 15000 ).getText();
         assertEquals("Встреча успешно забронирована на " + date, text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfEmptyCity() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='city']").$(".input__sub").getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+    @Test
+    void shouldNotSubmitIfEmptyDate() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $("[data-test-id='date']").$(".input_invalid").$(".input__sub").getText();
+        assertEquals("Неверно введена дата", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfEmptyName() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='name']").$(".input__sub").getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfEmptyPhone() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петров");
+//        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='phone']").$(".input__sub").getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfAgreementNotChecked() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("+79067778899");
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='agreement']").getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfInaccessibleCity() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Колыма");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='city']").$(".input__sub").getText();
+        assertEquals("Доставка в выбранный город недоступна", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfInaccessibleDate() {
+        LocalDate date = LocalDate.now();
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(formattedDate);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $("[data-test-id='date']").$(".input_invalid").$(".input__sub").getText();
+        assertEquals("Заказ на выбранную дату невозможен", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfLatinName() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Vasiliy Petrov");
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='name']").$(".input__sub").getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfNumeralsInName() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петр0в");
+        $("[name='phone']").setValue("+79067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='name']").$(".input__sub").getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfPhoneWithoutPlus() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("89067778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='phone']").$(".input__sub").getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldNotSubmitIfLettersInPhone() {
+        String date = generateDate();
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        SelenideElement dateForm = $("[data-test-id='date'] .input__control");
+        dateForm.sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        dateForm.setValue(date);
+        $("[name='name']").setValue("Василий Петров");
+        $("[name='phone']").setValue("+79О67778899");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        String text = $(".input_invalid[data-test-id='phone']").$(".input__sub").getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 }
